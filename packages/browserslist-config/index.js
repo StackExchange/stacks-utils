ac471dc99929451b8cdcd6230bdf3a78e8e539cc
Ben Kelly <b-kelly@users.noreply.github.com>
2022-08-16 21:17:55 -0400

    feat(*): initial commit

---
+++ packages/browserslist-config/index.js
@@ -0,0 +1,15 @@
+module.exports = [
+    "last 2 versions",
+    "not dead",
+    "not ie > 0",
+    "not op_mini all",
+    "not baidu > 0",
+    "not and_ff > 0",
+    "not and_qq > 0",
+    "not kaios > 0",
+    "not op_mob > 0",
+    "not ie_mob > 0",
+    "not and_uc > 0",
+    "not Samsung > 0",
+    "not Android > 0",
+];
---
+++ packages/eslint-config/index.js
@@ -0,0 +1,42 @@
+module.exports = {
+    plugins: ["@typescript-eslint", "jest"],
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "prettier",
+        "plugin:no-unsanitized/DOM",
+    ],
+    rules: {
+        "no-console": "error",
+        "no-alert": "error",
+        "no-process-env": "error",
+    },
+    overrides: [
+        {
+            // enable
+            files: ["**/*.ts", "**/*.tsx"],
+            parser: "@typescript-eslint/parser",
+            parserOptions: {
+                tsconfigRootDir: __dirname,
+            },
+        },
+        {
+            // only enable jest rules in non-e2e test files
+            files: ["**/!(*.e2e).test.ts"],
+            extends: ["plugin:jest/recommended", "plugin:jest/style"],
+            rules: {
+                "jest/no-disabled-tests": "off",
+                "jest/consistent-test-it": ["error", { fn: "it" }],
+            },
+        },
+        {
+            // enable a subset of jest rules in e2e test files, since the syntax is similar-ish
+            files: ["**/*.e2e.test.ts"],
+            extends: ["plugin:jest/recommended"],
+            rules: {
+                "jest/no-standalone-expect": "off",
+            },
+        },
+    ],
+};
